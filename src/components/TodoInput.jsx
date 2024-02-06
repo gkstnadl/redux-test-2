71be4c494bc5174cd6eee088bc4dd3517e41cd44
gkstnadl <gkstnadl12@naver.com>
2024-02-06 14:54:55 +0900

    뼈대만들기

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Todoinput() {
+  return (
+    <form>
+      <input />
+      <input />
+      <button>제출</button>
+    </form>
+  );
+}
+
+export default Todoinput;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+function TodoList() {
+  return (
+    <>
+      <div>TodoList</div>
+      <h1>todoTitle</h1>
+      <TodoItem></TodoItem>
+    </>
+  );
+}
+
+export default TodoList;
